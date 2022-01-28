459097b265a2bde282e8bb0061610d8676fecaed
cr10madrid007 <cristofermadrid2010@hotmail.com>
2022-01-27 23:18:55 -0600

    first commit

---
+++ Tarea1_2/Tarea1_2/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            var procs = new Models.Procesos
+            {
+                nombre = this.txtNombre.Text,
+                apellido = this.txtApellido.Text,
+                edad = Convert.ToInt32(this.txtEdad.Text),
+                correo = this.txtCorreo.Text
+
+            };
+            var pagina = new Page1();
+            pagina.BindingContext = procs;
+            await Navigation.PushAsync(pagina);
+
+        }
+    }
+}
---
+++ Tarea1_2/Tarea1_2/Models/Procesos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea1_2.Models
+{
+    public class Procesos
+    {
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public Int32 edad { get; set; }
+        public String correo { get; set; }
+    }
+}
